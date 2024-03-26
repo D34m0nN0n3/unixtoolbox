1c321291ba1e1bbd21f20f4a5f932423f6527236
Dmitriy Prigoda <46213676+D34m0nN0n3@users.noreply.github.com>
2024-03-26 12:43:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
+import sys
 import json
 from setuptools import setup, find_packages
+
+sys.stderr.write(
+    """
+===============================
+Unsupported installation method
+===============================
+This version of mkdocs-material no longer supports
+installation with `python setup.py install`.
+Please use `python -m pip install .` instead.
+"""
+)
+sys.exit(1)
 
 # Load list of dependencies
 with open("requirements.txt") as data:
